c601ff4c07d07e3246e0401fe29ac7655002b573
Marius <mg931@live.com>
2020-12-17 12:48:03 +0000

    #3 - Add package with custom widgets

---
+++ lib/widgets/list_row.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_demo/model/api_user.dart';
+import 'package:flutter_bloc_demo/widgets/txt.dart';
+
+class ListRow extends StatelessWidget {
+  final ApiUser user;
+  ListRow({this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Txt(text: 'ID is ${user.id}'),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
