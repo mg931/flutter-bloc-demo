3c4f6e918b3ea1a9751e6dc4f9172be19e442969
Marius <mg931@live.com>
2020-12-17 15:28:49 +0000

    #4 - Add name and email to user list row

--- lib/model/api_user.dart
+++ lib/model/api_user.dart
@@ -7,18 +7,21 @@ String apiUserToJson(List<ApiUser> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class ApiUser {
-  ApiUser({this.id, this.name});
+  ApiUser({this.id, this.name, this.email});
 
   String id;
   String name;
+  String email;
 
   factory ApiUser.fromJson(Map<String, dynamic> json) => ApiUser(
         id: json["id"],
         name: json["name"],
+        email: json["email"],
       );
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
+        "email": name,
       };
 }
--- lib/widgets/user_list_item.dart
+++ lib/widgets/user_list_item.dart
@@ -3,18 +3,28 @@ import 'package:flutter_bloc_demo/model/api_user.dart';
 
 class UserText extends StatelessWidget {
   final ApiUser user;
-  UserText({
-    this.user
-  });
+  UserText({this.user});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        user.name,
-        style: TextStyle(
-          fontSize: 18,
-          color: Theme.of(context).textTheme.bodyText1.color,
-        ),
+    return new Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            user.name,
+            style: TextStyle(
+              color: Theme.of(context).textTheme.bodyText1.color,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            user.email,
+            style: TextStyle(
+              color: Theme.of(context).textTheme.bodyText1.color,
+            ),
+          ),
+        ],
       ),
     );
   }
